816371a085320ca0df92abf249e2077bb96c8b33
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-12-24 07:25:49 +0800

    【ch04】 打印位置参数1

--- sh.sh
+++ sh.sh
@@ -345,3 +345,15 @@ echo '$ETT' | awk '{print $0}';
 
 #双引号解析变量值传递给awk
 echo "$ETT" | awk '{print $0}';#传递给0号参数
+
+####
+# 打印位置参数1 
+####
+#传递1个参数
+sh p1.sh oldboy;
+
+# 传递2个参数，但 p1.sh 并未接收第二个参数
+sh p1.sh oldboy oldgirl;
+
+#传递带空格的字符串参数
+sh p1.sh "oldboy oldgirl";
