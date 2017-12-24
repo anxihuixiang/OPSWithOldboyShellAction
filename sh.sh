9e058a900cbeafedcc9ee9e562662dc663722743
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-12-24 15:52:46 +0800

    【ch04】  代表执行脚本的名字或全路径名

--- sh.sh
+++ sh.sh
@@ -386,3 +386,21 @@ echo {a..z};
 
 #打钱前n个位置的字母
 sh n.sh {a..z};
+
+####
+#  $0 代表执行脚本的名字或全路径名 
+####
+#直接运行脚本名
+sh echo-0.sh
+
+#使用脚本全路径名执行
+sh ~/vspros/OPSWithOldboyShellAction/ch04/01-${n}/echo-0.sh
+
+# dirname 获取（脚本）路径的目录名
+dirname ~/vspros/OPSWithOldboyShellAction/ch04/01-${n}/echo-0.sh
+
+# basename 获取路径的文件名
+basename ~/vspros/OPSWithOldboyShellAction/ch04/01-${n}/echo-0.sh
+
+# 打印路径信息
+sh 05-echo-path.sh ~/vspros/OPSWithOldboyShellAction/ch04/01-${n}/echo-0.sh
