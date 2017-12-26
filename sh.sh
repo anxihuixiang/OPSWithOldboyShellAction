db7cdc66d65e81a67fc8678356727c75d462bd55
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-12-27 07:26:49 +0800

    【ch04】 shift 左移命令行参数（丢弃）

--- sh.sh
+++ sh.sh
@@ -687,3 +687,19 @@ echo ok;
 seq 5 >/tmp/tmp.log 
 
 sh 03-exec.sh
+
+####
+# shift 左移命令行参数（丢弃） 
+####
+#打印前2个命令行参数
+echo $1 $2
+if [ $# -eq 2 ] 
+then
+    #左移一个命令行参数  
+    shift
+    echo $1 #原先为 $2 的值
+fi
+
+# 测试 04-shift.sh
+sh 04-shift.sh 1 2
+
